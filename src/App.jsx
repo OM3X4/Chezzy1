30ce6153a880573ea13999d3ef3447740b7557fd
OM3X4 <om3x4e@gmail.com>
2024-10-16 15:53:55 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
+import { AiFillYoutube } from "react-icons/ai"; 
+import { BsTwitch } from "react-icons/bs"; 
+import { BsDiscord } from "react-icons/bs"; 
+import { AiOutlineInstagram } from "react-icons/ai"; 
+import { BsGooglePlay } from "react-icons/bs"; 
+import { AiFillApple } from "react-icons/ai"; 
+import { BsFacebook } from "react-icons/bs"; 
+import { FaChessBishop } from "react-icons/fa"; 
+/* eslint-disable*/
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router } from "react-router-dom"
+import { AnimatedBackground } from 'animated-backgrounds'
+import { Link } from "react-router-dom";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <AnimatedBackground  animationName="particleNetwork" style={{ filter: "brightness(60%) grayscale(100%)"}} />
+        <nav className=' bg-white bg-opacity-20  h-16 w-full px-60 text-center flex justify-center items-center gap-7'>
+          <div className="group items-end flex justify-center">
+            <FaChessBishop className=" text-white text-4xl group-hover:text-primary transition-all duration-150"/>
+            <Link to={"/"} className=" font-san font-extrabold text-3xl pl-5 text-primary group-hover:text-white transition-all duration-150">Cheezy</Link>
+          </div>
+          <Link className=" font-san font-extrabold text-3xl pl-5 text-primary hover:text-white transition-all duration-150">Play</Link>
+          <Link className=" font-san font-extrabold text-3xl pl-5 text-primary hover:text-white transition-all duration-150">Learn</Link>
+          <Link className=" font-san font-extrabold text-3xl pl-5 text-primary hover:text-white transition-all duration-150">Analyze</Link>
+          <Link className=" font-san font-extrabold text-3xl pl-5 text-primary hover:text-white transition-all duration-150">News</Link>
+        </nav>
+        <footer className=" bg-black w-full h-16 fixed bottom-0">
+            <div className=" flex justify-center items-center gap-2">
+              <p className=" text-white">©All Copy Rights Reserved</p>
+              <AiFillApple className=" text-white"/>
+              <BsGooglePlay className=" text-white"/>
+              <div></div>
+              <AiOutlineInstagram className=" text-white"/>
+              <BsDiscord className=" text-white"/>
+              <BsTwitch className=" text-white"/>
+              <BsFacebook className=" text-white"/>
+              <AiFillYoutube className=" text-white"/>
+            </div>
+        </footer>
     </>
   )
 }
